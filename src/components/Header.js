da6c568ab1fb537b3dcb692cfd606fee115b95de
Emmanuel-Develops <itakpeemma@gmail.com>
2021-03-05 20:49:59 +0100

    add button component in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
-const Header = ({ title }) => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Hello' />
+            <Button color='green' text='Hello 23' />
+            <Button color='green' text='Hello 21' />
         </header>
     )
 }
 
 Header.defaultProps = {
-    title: "Task Tracker"
+    title: "Task Tracker",
 }
+
+Header.propTypes = {
+    title : PropTypes.string.isRequired,
+}
+// CSS IN JS
+// const headerStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
+
 
 export default Header
